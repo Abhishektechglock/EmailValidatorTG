8cccd91b8b3961814e6469d830caddc14754630f
Abbierana <abhishek.rana@techglock.com>
2024-12-13 11:03:09 +0530

    framework added

---
+++ EmailValidatorTG/EmailValidatorTG.h
@@ -0,0 +1,18 @@
+//
+//  EmailValidatorTG.h
+//  EmailValidatorTG
+//
+//  Created by Techglock on 13/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmailValidatorTG.
+FOUNDATION_EXPORT double EmailValidatorTGVersionNumber;
+
+//! Project version string for EmailValidatorTG.
+FOUNDATION_EXPORT const unsigned char EmailValidatorTGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmailValidatorTG/PublicHeader.h>
+
+
